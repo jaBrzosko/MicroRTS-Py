bf8f0f2bef7dfdaad3bb563ca920155d75653227
jaBrzosko <jakubbrzoskowski@gmail.com>
2025-01-16 00:34:46 +0100

    push eval

--- mini/explore.py
+++ mini/explore.py
@@ -7,11 +7,13 @@ from agents.ppo_agent import PpoAgent
 import torch
 import torch.optim as optim
 import numpy as np
+import sys
 
 from gym_microrts import microrts_ai
+import argparse
 
 
-def train_from_parameters(params):
+def train_from_parameters(params, reward_weight):
     env = MicroRTSGridModeVecEnv(
         num_selfplay_envs=params.num_selfplay_envs,
         num_bot_envs=params.num_bot_envs,
@@ -20,7 +22,7 @@ def train_from_parameters(params):
         render_theme=2,
         ai2s=params.ais,
         map_paths=params.maps,
-        reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0]),
+        reward_weight=reward_weight,
         # cycle_maps=args.train_maps,
     )
 
@@ -41,6 +43,16 @@ def train_from_parameters(params):
         train(env, params, agent, optimizer)
 
 if __name__ == "__main__":
+
+    def parse_args():
+        parser = argparse.ArgumentParser(description="MicroRTS Training Script")
+        parser.add_argument('--reward-weight', type=float, nargs='+', default=[10.0, 1.0, 1.0, 0.2, 1.0, 4.0],
+                            help='Reward weights for the environment')
+        return parser.parse_args()
+
+    args = parse_args()
+    reward_weight = np.array(args.reward_weight)
+
     params = Parameters()
     params.num_selfplay_envs = 4
     params.num_bot_envs = 8
@@ -48,4 +60,4 @@ if __name__ == "__main__":
     params.ais = [microrts_ai.POLightRush, microrts_ai.coacAI, microrts_ai.naiveMCTSAI, microrts_ai.rojo,
                 microrts_ai.izanagi, microrts_ai.tiamat, microrts_ai.mixedBot, microrts_ai.POWorkerRush]
     print("Training selfplay and bots")
-    train_from_parameters(params)
+    train_from_parameters(params, reward_weight)
